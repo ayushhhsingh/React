2750e9b10aeccb7c735f7ff957e17f415f063f62
ayushhhsingh <ayush72350@gmail.com>
2025-11-03 01:50:12 +0530

    hooks

---
+++ customreact/react.js
@@ -0,0 +1,20 @@
+function customRender(reactElement, Container) {
+  const domElement = document.createElement(reactElement.type);
+  domElement.innerHTML = reactElement.children;
+  domElement.setAttribute("href", reactElement.props.href);
+  domElement.setAttribute("target", reactElement.props.target);
+  Container.appendChild(domElement);
+}
+
+const reactElement = {
+  type: "a",
+  props: {
+    href: "https:/google.com",
+    target: "_blank",
+  },
+  children: "click me to visit google",
+};
+
+const mainContainer = document.querySelector("#root");
+
+customRender(reactElement, mainContainer);
