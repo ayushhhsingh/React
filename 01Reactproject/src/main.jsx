70854384047e24accef8145fbd93486d1369ddec
ayushhhsingh <ayush72350@gmail.com>
2025-11-02 23:21:39 +0530

    tried custom react

--- 01Reactproject/src/main.jsx
+++ 01Reactproject/src/main.jsx
@@ -2,7 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App.jsx";
-
+function Myapp() {
+  return (
+    <div>
+      <h1>hello react with js</h1>
+    </div>
+  );
+}
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
