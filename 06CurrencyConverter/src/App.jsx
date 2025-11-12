df70bde0f72f7d0b86f6dd208a645d528c7086e9
ayushhhsingh <ayush72350@gmail.com>
2025-11-12 23:01:54 +0530

    fix tailwind

--- 06CurrencyConverter/src/App.jsx
+++ 06CurrencyConverter/src/App.jsx
@@ -1,15 +1,11 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <body className="bg-slate-800">
-        <h1 className="bg-amber-950 text-amber-300">Test for tailwind</h1>
-      </body>
+      <h1 className="bg-black text-amber-300">Test for tailwind</h1>
     </>
   );
 }
