1202bb52ce63d287b8fdc9476b28c6c601448de6
ayushhhsingh <ayush72350@gmail.com>
2025-11-13 00:05:53 +0530

    basic bg

--- 06CurrencyConverter/src/App.jsx
+++ 06CurrencyConverter/src/App.jsx
@@ -5,7 +5,14 @@ function App() {
 
   return (
     <>
-      <h1 className="bg-black text-amber-300">Test for tailwind</h1>
+      <body
+        className="w-full max-h-screen flex justify-center items-center bg-cover bg-no-repeat flex-wrap"
+        style={{
+          backgroundImage: `url(https://images.pexels.com/photos/534216/pexels-photo-534216.jpeg)`,
+        }}
+      >
+        <h1 className="bg-red-200">test for tailwind</h1>
+      </body>
     </>
   );
 }
