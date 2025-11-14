9f7ebe14fcb86afc3e425e2a5826323cf759bf31
ayushhhsingh <ayush72350@gmail.com>
2025-11-14 21:53:33 +0530

    bg changed

--- 06CurrencyConverter/src/App.jsx
+++ 06CurrencyConverter/src/App.jsx
@@ -5,14 +5,14 @@ function App() {
 
   return (
     <>
-      <body
-        className="w-full max-h-screen flex justify-center items-center bg-cover bg-no-repeat flex-wrap"
+      <div
+        className="w-full h-screen flex justify-center items-center bg-cover bg-no-repeat bg-center"
         style={{
-          backgroundImage: `url(https://images.pexels.com/photos/534216/pexels-photo-534216.jpeg)`,
+          backgroundImage: `url('/src/assets/currency.jpeg')`,
         }}
       >
         <h1 className="bg-red-200">test for tailwind</h1>
-      </body>
+      </div>
     </>
   );
 }
