63bb29602a640c847954d22b43d50442758ad465
ayushhhsingh <ayush72350@gmail.com>
2025-11-14 23:46:45 +0530

    added hooks

---
+++ 06CurrencyConverter/src/hooks/useCurrencyhooks.js
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+
+function UseCurrencyINfo(currency) {
+  const [data, SetData] = useState({});
+  useEffect(() => {
+    fetch(
+      `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
+    )
+      .then((res) => res.json)
+      .then((res) => SetData(res[currency]));
+  }, [currency]);
+
+  return data;
+}
