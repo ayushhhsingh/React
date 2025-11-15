14455e9f6519eccdb722374b62d8cf9f6dac68cf
ayushhhsingh <ayush72350@gmail.com>
2025-11-16 00:41:19 +0530

    added routes

--- 07ReactRouter/src/main.jsx
+++ 07ReactRouter/src/main.jsx
@@ -11,7 +11,12 @@ import {
 } from "react-router-dom";
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route path="/" element={<Layout />} />)
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<div>home</div>} />
+      <Route path="about" element={<div>about</div>} />
+    </Route>
+  )
 );
 
 createRoot(document.getElementById("root")).render(
