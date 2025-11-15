5a8d21c4d58b295ea578c22c5c1ee3d09920660e
ayushhhsingh <ayush72350@gmail.com>
2025-11-15 19:02:28 +0530

    added router

--- 07ReactRouter/src/main.jsx
+++ 07ReactRouter/src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Layout from "./Layout.jsx";
 
-createRoot(document.getElementById('root')).render(
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path="/" element={<Layout />} />)
+);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
