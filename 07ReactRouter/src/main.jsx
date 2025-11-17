81fe2cab1ace0da29fa65d77e02c42c9759e5d89
ayushhhsingh <ayush72350@gmail.com>
2025-11-17 21:52:16 +0530

    fixed bugs

--- 07ReactRouter/src/main.jsx
+++ 07ReactRouter/src/main.jsx
@@ -1,24 +1,25 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import Layout from "./Layout.jsx";
-
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
   RouterProvider,
 } from "react-router-dom";
+import Home from "./components/home/home.jsx";
+import About from "./components/about/about.jsx";
+// import User from "./components/User/User.jsx";
+// import Github, { githubInfoLoader } from "./components/Github/Github.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
-      <Route index element={<div>home</div>} />
-      <Route path="about" element={<div>about</div>} />
+      <Route path="" element={<Home />} />
+      <Route path="About" element={<About />} />
     </Route>
   )
 );
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
