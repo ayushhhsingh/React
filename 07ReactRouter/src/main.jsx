73f18ce013993d20718ce6cb79a2a240ed330dee
ayushhhsingh <ayush72350@gmail.com>
2025-11-18 01:35:37 +0530

    project completed

--- 07ReactRouter/src/main.jsx
+++ 07ReactRouter/src/main.jsx
@@ -11,15 +11,18 @@ import Home from "./components/home/home.jsx";
 import About from "./components/about/about.jsx";
 import App from "./App.jsx";
 import "./index.css";
-
-// import User from "./components/User/User.jsx";
-// import Github, { githubInfoLoader } from "./components/Github/Github.jsx";
+import User from "./components/User/User.jsx";
+import Github, { githubInfoLoader } from "./components/Github/Github.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
       <Route path="" element={<Home />} />
       <Route path="About" element={<About />} />
+      <Route path="user/" element={<User />} />
+      <Route path=":userid" element={<User />} />
+      <Route loader={githubInfoLoader} path="Github" element={<Github />} />
+      <Route path="*" element={<h1>Not Found</h1>} />
     </Route>
   )
 );
