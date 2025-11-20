eaca89e6b684923a7fbf22fa7f442d8b58f1ee48
ayushhhsingh <ayush72350@gmail.com>
2025-11-20 23:36:03 +0530

    bug in profile

--- 08miniContext/src/components/profile.jsx
+++ 08miniContext/src/components/profile.jsx
@@ -1,16 +1,15 @@
-import React, {useContext} from 'react'
-import UserContext from '../context/UserContext'
+import React, { useContext } from "react";
+import UserContext from "../context/UserContext.jsx";
 
 function Profile() {
-  const {user} = useContext(UserContext)
+  const { user } = useContext(UserContext);
 
-  if(!user) return <h1>Not logged in </h1>
+  if (!user) return <h1>Not logged in </h1>;
   return (
     <div>
-
-        <h1>Profile : {user.username}</h1>
+      <h1>Profile : {user.username}</h1>
     </div>
-  )
+  );
 }
 
-export default Profile
+export default Profile;
