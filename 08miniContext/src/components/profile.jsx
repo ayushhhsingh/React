762c8800bfcbf9b27b378d07e9882c079fee5495
ayushhhsingh <ayush72350@gmail.com>
2025-11-21 01:44:07 +0530

    profile boilerplate code

--- 08miniContext/src/components/profile.jsx
+++ 08miniContext/src/components/profile.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import UserContext from "../context/UserContext.jsx";
+import UserContext from "../context/UserConstext.js";
 
 function Profile() {
   const { user } = useContext(UserContext);
